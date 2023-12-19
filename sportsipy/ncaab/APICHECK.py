a0104516c04f57e1ee8e5d7391bfba9613a75795
shmoopthegoat <143813869+shmoopthegoat@users.noreply.github.com>
2023-12-19 09:49:31 -0500

    Update APICHECK.py

---
+++ sportsipy/ncaab/APICHECK.py
@@ -0,0 +1,10 @@
+from ratelimit import limits, sleep_and_retry
+
+CALLS = 1
+RATE_LIMIT = 60
+
+@sleep_and_retry
+@limits(calls = CALLS, period = RATE_LIMIT)
+def check_limit():
+    'empty'
+return
